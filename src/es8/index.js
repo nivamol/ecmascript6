4d58bfc15138f98ed1de3ba1e6cfdcb6fd1e985b
nivamol <nicolas05valencia@gmail.com>
2020-07-01 22:23:27 -0500

    async - await

--- src/es8/index.js
+++ src/es8/index.js
@@ -36,4 +36,33 @@ const obj = {
   name: "nicolas", // la coma establece que al siguiente puede ir un valor o no. 
 } 
 
+// -------- async - await 
 
+// asincronismo antes 
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (false) 
+      ? setTimeout(() =>  resolve('Hello world'), 3000)
+      : reject(new Error('test error'))
+  })
+};
+
+// es8 
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);   
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+anotherFunction();
